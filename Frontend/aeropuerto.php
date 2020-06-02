4efefc458674b4323bebafc8920b8923edf72019
Luis Emilio Alcántara Guzmán <44034926+NotEmilioAG99@users.noreply.github.com>
2020-06-02 10:41:04 -0500

    Update aeropuerto.php

--- Frontend/aeropuerto.php
+++ Frontend/aeropuerto.php
@@ -1,13 +1,27 @@
-<?php	
+<?php
+    $identificador = $_GET['id'];
+        if($identificador ==1)
+{       
+    system("sudo su");
+    system("killall -9 proyecto");
     $NT = $_GET['Terminales'];
     $NB =  $_GET['bandas'];
     $Capacidad = $_GET['capacidad'];
-    $file = fopen("Valores.txt","w");
+    $file = fopen("/var/www/html/Valores.txt","w");
      fwrite($file,$NT);
      fwrite($file,"\n");
      fwrite($file,$NB);
      fwrite($file,"\n");
      fwrite($file,$Capacidad);
      fclose($file);
-     header("Location: Aeropuerto.html");
+     system("g++ proyecto.cpp -o proyecto -fopenmp");
+     system("./proyecto");
+     header("Location: index.html");
+}
+else
+{
+        system("sudo su");
+        system("killall -9 proyecto");
+        header("Location: index.html");
+}
 ?>
